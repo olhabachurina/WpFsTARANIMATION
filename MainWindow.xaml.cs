ce4320275b158d2965a525229e59907ee7cc7cc8
Olga Bachurina <bachuri12@ukr.net>
2023-12-05 23:14:17 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpFsTARANIMATION
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            StartAnimation();
+        }
+
+        private void StartAnimation()
+        {
+            DoubleAnimation animation = new DoubleAnimation
+            {
+                From = 0,     
+                To = 650,     
+                Duration = TimeSpan.FromSeconds(1),
+                AutoReverse = true,
+                RepeatBehavior = RepeatBehavior.Forever
+            };
+
+            animatedImage.BeginAnimation(Canvas.LeftProperty, animation);
+        }
+    }
+}
+
